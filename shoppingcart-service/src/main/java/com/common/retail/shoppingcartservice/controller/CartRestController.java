5686b9b36f95a57f3ce5ad34e8ccc89ed41b831b
Priyanka <pk266394@gmail.com>
2019-12-12 16:45:16 +0530

    done

---
+++ shoppingcart-service/src/main/java/com/common/retail/shoppingcartservice/controller/CartRestController.java
@@ -0,0 +1,19 @@
+package com.common.retail.shoppingcartservice.controller;
+
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CartRestController {
+
+    @RequestMapping("/shopping")
+    public String welcome() {
+        return "Welcome to shopping cart";
+    }
+
+    @RequestMapping("/shopping/cart")
+    public String Cart() {
+        return "Welcome to cart";
+    }
+}
